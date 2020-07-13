d0f6bb06f1f7664fb3d355b5c33c523390d795dd
Spryt <spryt_twitch@hotmail.com>
2020-07-13 19:44:12 +0100

    Uploading existing project files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public BoxCollider PlayerCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+
+    }
+}
