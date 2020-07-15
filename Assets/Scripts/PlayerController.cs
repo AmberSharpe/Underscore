176c65d80a15b687df6f07591c1e26152f9c0fa7
Spryt <spryt_twitch@hotmail.com>
2020-07-15 16:22:16 +0100

    Ground check completed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.EventSystems;
+﻿using UnityEngine;
 
-[SerializeField]
 enum State
 {
-    Solid,Liquid,Gas
+    Solid,
+    Liquid,
+    Gas
 }
 
 public class PlayerController : MonoBehaviour
 {
+    private bool isGrounded
+    {
+        get
+        {
+            Vector2 boxSize = new Vector2(boxCollider2d.size.x, 0.05f);
+
+            int boxOverlap = Physics2D.OverlapBox(
+                new Vector2(transform.position.x, transform.position.y) + Vector2.down * (boxCollider2d.bounds.extents.y + boxSize.y),
+                boxSize,
+                0f,
+                new ContactFilter2D()
+                {
+                    layerMask = LayerMask.GetMask("Ground"),
+                    useLayerMask = true
+                },
+                new Collider2D[1]);
+
+            return boxOverlap > 0;
+        }
+    }
+
     //References
     [SerializeField]
     private Rigidbody2D rb;
+    private BoxCollider2D boxCollider2d;
+
 
     //State
     [SerializeField]
@@ -26,27 +47,28 @@ public class PlayerController : MonoBehaviour
     public float solidJumpHeight = 10;
 
     const float solidMaxHorizontalSpeed = 10;
-    const float solidMaxVerticalSpeed = 10;    
-    
+    const float solidMaxVerticalSpeed = 10;
+
     const float liquidMaxHorizontalSpeed = 10;
-    const float liquidMaxVerticalSpeed = 10;    
+    const float liquidMaxVerticalSpeed = 10;
 
     const float gasMaxHorizontalSpeed = 10;
     const float gasMaxVerticalSpeed = 10;
-    
+
     //Input
     private bool jump;
     private float horizontalInput;
     private float verticalInput;
 
     //Movement
-    private float moveDirectionMag;        
+    private float moveDirectionMag;
     public Vector2 moveDirection;
     public float baseMoveAcceleration = 10;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        boxCollider2d = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
@@ -69,15 +91,14 @@ public class PlayerController : MonoBehaviour
 
     private void HandleMovement()
     {
-
-    switch (currentState)
-    {                
+        switch (currentState)
+        {
             case State.Solid://Solid                              
                 moveDirection = (Vector3.right * horizontalInput).normalized;
                 moveDirectionMag = Mathf.Clamp01(Mathf.Abs(horizontalInput));
 
-                if (jump)
-                {                 
+                if (jump && isGrounded)
+                {
                     rb.velocity += Vector2.up * rb.mass * Mathf.Sqrt(2 * Mathf.Abs(gravity) * solidJumpHeight);
                 }
 
@@ -87,10 +108,10 @@ public class PlayerController : MonoBehaviour
 
                 //Clamp Speed
                 rb.velocity = new Vector2(
-                    Mathf.Clamp(rb.velocity.x,-solidMaxHorizontalSpeed,solidMaxHorizontalSpeed), 
-                    Mathf.Clamp(rb.velocity.y,-solidMaxVerticalSpeed,solidMaxVerticalSpeed)
+                    Mathf.Clamp(rb.velocity.x, -solidMaxHorizontalSpeed, solidMaxHorizontalSpeed),
+                    Mathf.Clamp(rb.velocity.y, -solidMaxVerticalSpeed, solidMaxVerticalSpeed)
                     );
-                break;     
+                break;
 
             case State.Liquid: //Liquid                                      
                 moveDirection = (Vector3.right * horizontalInput).normalized;
@@ -101,11 +122,11 @@ public class PlayerController : MonoBehaviour
 
                 //Clamp Speed
                 rb.velocity = new Vector2(
-                    Mathf.Clamp(rb.velocity.x,-liquidMaxHorizontalSpeed, liquidMaxHorizontalSpeed), 
-                    Mathf.Clamp(rb.velocity.y,-liquidMaxVerticalSpeed, liquidMaxVerticalSpeed)
+                    Mathf.Clamp(rb.velocity.x, -liquidMaxHorizontalSpeed, liquidMaxHorizontalSpeed),
+                    Mathf.Clamp(rb.velocity.y, -liquidMaxVerticalSpeed, liquidMaxVerticalSpeed)
                     );
-                break;      
-                
+                break;
+
             case State.Gas://Gas                               
                 moveDirection = (Vector3.right * horizontalInput + Vector3.up * verticalInput).normalized;
                 moveDirectionMag = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
@@ -115,12 +136,10 @@ public class PlayerController : MonoBehaviour
 
                 //Clamp Speed
                 rb.velocity = new Vector2(
-                    Mathf.Clamp(rb.velocity.x,-gasMaxHorizontalSpeed,gasMaxHorizontalSpeed), 
-                    Mathf.Clamp(rb.velocity.y,-gasMaxVerticalSpeed,gasMaxVerticalSpeed)
+                    Mathf.Clamp(rb.velocity.x, -gasMaxHorizontalSpeed, gasMaxHorizontalSpeed),
+                    Mathf.Clamp(rb.velocity.y, -gasMaxVerticalSpeed, gasMaxVerticalSpeed)
                     );
                 break;
-    }
-               
-
+        }
     }
 }
